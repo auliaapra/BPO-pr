090359e755b2d72d3bccf28341a3b1c19b3b17a1
Aulia Putri Ramadhani <aulia.putri.tif23@polban.ac.id>
2024-08-30 07:38:16 +0700

    rename

---
+++ P2S5String.java
@@ -0,0 +1,23 @@
+public class String {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String A = scanner.nextLine();
+        String B = scanner.nextLine();
+
+        int totalLength = A.length() + B.length();
+        System.out.println(totalLength);
+
+        if (A.compareTo(B) > 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+        String capitalizedA = A.substring(0, 1).toUpperCase() + A.substring(1);
+        String capitalizedB = B.substring(0, 1).toUpperCase() + B.substring(1);
+        System.out.println(capitalizedA + " " + capitalizedB);
+
+        scanner.close();
+    }
+}
